fc41feef9c4c443ac83dd8874c4032e21c48f412
Woncheol Kim <icijiy@gmail.com>
2022-08-21 07:58:39 -0700

    Add charactor and new tweet, button hover

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,13 @@
 $(document).ready( () => {
   $('textarea').on('input', function() {
     // This selects the input text and then finds its length
-    let inputLength = $(this).val().length
+    let leftLength = 140 - $(this).val().length;
     // This selects the parent of the textarea (form) then finds the output tag with find()
       // ^that selects the node, to get the text you do .text()      
-    let counterValue = $(this).parent().find('output').text(140 - inputLength);
+    let counterValue = $(this).parent().find('output').text(leftLength);
     
-    // if (Number(counterValue.text()) < 0) {
-    //   console.log(counterValue.text())
-    //   counterValue.css('color', 'red')
-    // } else {
-    //   counterValue.css('color', '')
-    // }
-    if (Number(counterValue.text()) < 0) {
-      console.log(counterValue.text())
+    if (leftLength < 0) {
+      console.log(leftLength)
       counterValue.addClass('counter-red')
     } else {
       counterValue.removeClass('counter-red')
