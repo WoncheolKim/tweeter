b4fbf3b146b86336592f1a82dcc82113e4139f4d
Woncheol Kim <icijiy@gmail.com>
2022-08-21 06:04:39 -0700

    add style form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,15 +27,38 @@ $('#tweets-container').append($tweet); // to add it to the page so we can make s
 
 
 const renderTweets = function(tweets) {
+  $('#tweets-container').empty();
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
+  for (const tweet in tweets) {
+    let $tweet = createTweetElement(tweets[tweet]);
+    $('#tweets-container').prepend($tweet);
+  }
 }
 
 const createTweetElement = function(tweet) {
-  let $tweet = /* Your code for creating the tweet element */
-  // ...
-  return $tweet;
+  let $tweet = `  
+  <article>
+    <header>
+      <div>
+      <img src=${tweetData.user.avatars} alt="profile icon">
+        <p class="person-name">${tweetData.user.name}</p>
+      </div>
+      <div>${tweetData.user.handle}</div>
+    </header>
+    <p>${tweetData.content.text}</p>
+    <footer class="tweet-footer">
+      <div >${timeago.format(tweetData.created_at)}</div>
+      <div class="tweet-footer-right">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+  `
+  return $tweet
 }
 
 renderTweets(data);
