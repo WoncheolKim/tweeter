4d532a454960624d3e77089edd2c507f2395d692
Woncheol Kim <icijiy@gmail.com>
2022-08-22 22:38:23 -0700

    fixed errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$( document ).ready(function() {
+  //all of the code here
+     
 
 const createTweetElement = function(tweetData) {
   return `  
@@ -27,7 +30,68 @@ const createTweetElement = function(tweetData) {
   `
 }
 
+const loadTweets = function () {
+  $("#new-tweet").slideUp("fast");
+  $.ajax({
+    method: "GET",
+    url: "/tweets",
+    success: (response) => {
+      console.log(response);
+      renderTweets(response.reverse());
+    },
+    error: (error) => {
+      console.log(error);
+    }
+});
+};
+
+$('#submitTweet').submit(function(event) {
+  event.preventDefault();
+  const textData = $(this).serialize();
+  const tweetLength = $('#tweet-text').val().length;
+  if (tweetLength === 0) {
+    $("#alertMessage").slideDown("slow", creatAlertMessage("emptyAlert"));
+  } else {
+    if (tweetLength > 140) {
+      $("#alertMessage").slideDown("slow", creatAlertMessage("lengthAlert"));
+    } else {
+      $('#tweet-text').val("");
+      $('#counter').val('140');
+      $.ajax({
+        method: "POST",
+        url: "/tweets/",
+        data: textData,
+        success: () => {
+          loadTweets();
+          alert('succssfulypostedtweet');
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
+  }
+});
+
+const creatAlertMessage = function(message) {
+  $("#alertMessage").empty();
+  if (message === "emptyAlert") {
+    const alert = `<i class="fa-solid fa-triangle-exclamation"></i>
+    Tweet contents should not be empty.
+      <i class="fa-solid fa-triangle-exclamation"></i>`;
+    $("#alertMessage").append(alert);
+  }
+
+  if (message === "lengthAlert") {
+    const alert = `<i class="fa-solid fa-triangle-exclamation"></i>
+    Too long. Tweet contents should not exceed 140 letters.
+      <i class="fa-solid fa-triangle-exclamation"></i>`;
+    $("#alertMessage").append(alert);
+  }
+};
+
 const renderTweets = function(tweets) {
+  console.log("renderingtweet")
   $('#tweets-container').empty();
   for (const tweet of tweets) {
     let $tweet = createTweetElement(tweet);
@@ -35,58 +99,27 @@ const renderTweets = function(tweets) {
   }
 }
 
-const loadTweets = function () {
-  $('#submitTweet').submit(function() {
-    $.ajax({
-      url: "/tweets",
-      success: (response) => {
-        renderTweets(response);
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    });
-  });
-};
-
-$('#submitTweet').submit(function(event) {
-  event.preventDefault();
-  console.log($(this).serialize());
-  const textData = $(this).serialize();
-  $.ajax({
-    method: "POST",
-    url: "/tweets",
-    data: textData,
-    error: (error) => {
-      console.log(error);
-    }
-  });
+$('#tweet-text').click(() => {
+  $("#alertMessage").empty();
+  $("#alertMessage").slideUp("fast");
 });
 
-loadTweets();
+$("#navAngleIcon").click(function() {
+  $("#new-tweet").slideToggle("slow");
+  $("#tweet-text").focus();
+});
 
+$(window).scroll(function() {
+  console.log($(document).scrollTop())
+  if ($(document).scrollTop() > 500) {
+    $("#scrollUpButton").css("display", "block");
+  } else {
+    $("#scrollUpButton").css("display", "none");
+  }
+});
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweets = [{
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-},{
-  "user": {
-    "name": "Json",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@OurJson"
-    },
-  "content": {
-      "text": "I qwrkwqeknte rkwenf"
-    },
-  "created_at": 1461116232227
-}];
+$("#scrollUpButton").on("click", function() {
+  $(document).scrollTop(0);
+});
 
-renderTweets(tweets);
+});
